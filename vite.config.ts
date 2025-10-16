3cfdb44f8382c5599eab1ebdfd65f37e414d2dc7
drvntechnologies <135929237+drvntechnologies@users.noreply.github.com>
2025-10-16 08:52:46 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',  // Root path for custom domain deploys
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
